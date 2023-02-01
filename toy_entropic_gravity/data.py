b83f35cc4e16508ae2ba1fb45cba009d737e9ce4
Alex Davis <abd516@york.ac.uk>
2023-02-01 22:54:14 +0000

    Changed python code to try a couple of different regression models to find best one.

--- toy_entropic_gravity/data.py
+++ toy_entropic_gravity/data.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import r2_score
 
 data = np.loadtxt('./output/230131164340.csv', delimiter=',')
@@ -19,19 +19,27 @@ y = np.log(y)
 y_train = y[:train_size]
 y_test = y[train_size:]
 
-reg = LinearRegression()
-reg.fit(x_train, y_train)
-
-y_pred = reg.predict(x_test)
-m = reg.coef_[0]
-c = reg.intercept_
-
-print('Gradient:', m)
-print('Intercept:', c)
-print('Coefficient of determination:', r2_score(y_test, y_pred))
-
 fig, ax = plt.subplots()
 ax.scatter(x, y)
-ax.plot(x, c + m*x, color='red', linestyle='-')
+
+models = LinearRegression, Ridge, Lasso
+model_names = 'Linear regression', 'Ridge regression', 'Lasso regression'
+colours = 'red', 'green', 'blue'
+
+for i, model in enumerate(models):
+    reg = model()
+    reg.fit(x_train, y_train)
+
+    y_pred = reg.predict(x_test)
+    m = reg.coef_[0]
+    c = reg.intercept_
+    ax.plot(x, c + m*x, color=colours[i], linestyle='-')
+
+    print(model_names[i] + ":")
+    print('Gradient:', m)
+    print('Intercept:', c)
+    print('Coefficient of determination:', r2_score(y_test, y_pred))
+    print('')
+
 plt.text(1, 17, 'ln(F) = {0:.2f} + {1:.2f}ln(1/x)'.format(c, m), size=14)
 plt.show()
